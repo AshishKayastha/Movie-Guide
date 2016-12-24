889f42034a01ef4a3ae71bba7d505274a5f9ae84
Ashish Kayastha <mylife2048@gmail.com>
2016-12-24 21:49:01 +0545

    Initial Commit

---
+++ app/src/main/java/com/ashish/movies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ashish.movies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
