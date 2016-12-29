525a1aa81ab17fca2d6e4a94ec6fe9644a0488b5
Ashish Kayastha <ashishkayastha.smartmobe@gmail.com>
2016-12-29 14:04:41 +0545

    Initialize Timber for debug and release builds

--- app/src/main/kotlin/com/ashish/movies/app/MoviesApp.kt
+++ app/src/main/kotlin/com/ashish/movies/app/MoviesApp.kt
@@ -2,19 +2,23 @@ package com.ashish.movies.app
 
 import android.app.Application
 import android.content.Context
+import android.support.v7.app.AppCompatDelegate
+import com.ashish.movies.BuildConfig
 import com.ashish.movies.di.components.AppComponent
 import com.ashish.movies.di.components.DaggerAppComponent
+import com.ashish.movies.utils.ReleaseTree
+import timber.log.Timber
 
 /**
  * Created by Ashish on Dec 28.
  */
 class MoviesApp : Application() {
 
-    private val appComponent: AppComponent by lazy {
-        DaggerAppComponent.builder().build()
-    }
-
     companion object {
+        init {
+            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        }
+
         fun getAppComponent(context: Context): AppComponent {
             val app = context.applicationContext as MoviesApp
             return app.appComponent
@@ -23,5 +27,22 @@ class MoviesApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initTimber()
+    }
+
+    private val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder().build()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        } else {
+            Timber.plant(ReleaseTree())
+        }
     }
 }
