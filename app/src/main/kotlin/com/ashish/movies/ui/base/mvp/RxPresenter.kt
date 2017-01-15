fa4bd72cd115002621f92f5bad92e6bbce27e3b8
Ashish Kayastha <mylife2048@gmail.com>
2017-01-15 15:07:25 +0545

    Code  cleanup

--- app/src/main/kotlin/com/ashish/movies/ui/base/mvp/RxPresenter.kt
+++ app/src/main/kotlin/com/ashish/movies/ui/base/mvp/RxPresenter.kt
@@ -1,10 +1,8 @@
 package com.ashish.movies.ui.base.mvp
 
 import android.support.annotation.CallSuper
-import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.subjects.BehaviorSubject
 
 /**
  * Created by Ashish on Dec 28.
@@ -12,13 +10,11 @@ import io.reactivex.subjects.BehaviorSubject
 abstract class RxPresenter<V : MvpView> {
 
     private var view: V? = null
-    private val viewLifecycle = BehaviorSubject.create<Boolean>()
     private var compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     @CallSuper
     open fun attachView(view: V) {
         this.view = view
-        viewLifecycle.onNext(true)
     }
 
     protected fun getView() = view
@@ -30,15 +26,10 @@ abstract class RxPresenter<V : MvpView> {
     @CallSuper
     open fun detachView() {
         view = null
-        viewLifecycle.onNext(false)
     }
 
     @CallSuper
     open fun onDestroy() {
         compositeDisposable.clear()
-    }
-
-    protected fun getViewState(): Observable<Boolean> {
-        return viewLifecycle
     }
 }
