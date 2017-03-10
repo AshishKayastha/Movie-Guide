f455e468b917491fd1e35b07f70e9e9e2d00649d
Ashish Kayastha <mylife2048@gmail.com>
2017-03-10 19:02:55 +0545

    Removed database related code for now. It's not completed yet so will be merged later from database branch

--- app/src/main/kotlin/com/ashish/movieguide/di/modules/AppModule.kt
+++ app/src/main/kotlin/com/ashish/movieguide/di/modules/AppModule.kt
@@ -2,24 +2,8 @@ package com.ashish.movieguide.di.modules
 
 import android.app.Application
 import android.content.Context
-import android.database.sqlite.SQLiteOpenHelper
-import com.ashish.movieguide.data.database.DatabaseHelper
-import com.ashish.movieguide.data.database.entities.CreditEntity
-import com.ashish.movieguide.data.database.entities.CreditEntitySQLiteTypeMapping
-import com.ashish.movieguide.data.database.entities.GenreEntity
-import com.ashish.movieguide.data.database.entities.GenreEntitySQLiteTypeMapping
-import com.ashish.movieguide.data.database.entities.ImageEntity
-import com.ashish.movieguide.data.database.entities.ImageEntitySQLiteTypeMapping
-import com.ashish.movieguide.data.database.entities.MovieDetailEntity
-import com.ashish.movieguide.data.database.entities.MovieEntity
-import com.ashish.movieguide.data.database.entities.VideoEntity
-import com.ashish.movieguide.data.database.entities.VideoEntitySQLiteTypeMapping
-import com.ashish.movieguide.data.database.resolvers.movie.MovieSQLiteTypeMapping
-import com.ashish.movieguide.data.database.resolvers.moviedetail.MovieDetailSQLiteTypeMapping
 import com.ashish.movieguide.di.qualifiers.ApplicationContext
 import com.ashish.movieguide.utils.extensions.defaultSharedPreferences
-import com.pushtorefresh.storio.sqlite.StorIOSQLite
-import com.pushtorefresh.storio.sqlite.impl.DefaultStorIOSQLite
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -36,23 +20,5 @@ class AppModule(private val application: Application) {
 
     @Provides
     @Singleton
-    fun provideSQLiteOperHelper(@ApplicationContext context: Context): SQLiteOpenHelper = DatabaseHelper(context)
-
-    @Provides
-    @Singleton
     fun provideSharedPrefs(@ApplicationContext context: Context) = context.defaultSharedPreferences
-
-    @Provides
-    @Singleton
-    fun provideStorIOSQlite(sqLiteOpenHelper: SQLiteOpenHelper): StorIOSQLite {
-        return DefaultStorIOSQLite.builder()
-                .sqliteOpenHelper(sqLiteOpenHelper)
-                .addTypeMapping(MovieEntity::class.java, MovieSQLiteTypeMapping())
-                .addTypeMapping(GenreEntity::class.java, GenreEntitySQLiteTypeMapping())
-                .addTypeMapping(ImageEntity::class.java, ImageEntitySQLiteTypeMapping())
-                .addTypeMapping(VideoEntity::class.java, VideoEntitySQLiteTypeMapping())
-                .addTypeMapping(CreditEntity::class.java, CreditEntitySQLiteTypeMapping())
-                .addTypeMapping(MovieDetailEntity::class.java, MovieDetailSQLiteTypeMapping())
-                .build()
-    }
 }
